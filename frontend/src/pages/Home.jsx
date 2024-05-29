5b69b55d768db8d8b86a3a16bd22304e77651d46
Varad Kakodkar <varad25gt@gmail.com>
2024-05-29 08:25:34 +0000

    vite.config.js changed and home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
       <div className="m-auto">
         <div className="startScreen" style={{ backgroundImage: `url(${home_page})` }}>
           <div id="nameg" style={{ fontFamily: 'Arial' }}>Connect Four</div>
-          <Link to="/connect4" className="btn btn-light" style={{ padding: '1em 4em', fontSize: '1.4em', fontFamily: 'Arial' }}>Start</Link>
+          <Link to="/connect4" className="btn btn-light" style={{ padding: '1em 4em', fontSize: '1.35em', fontFamily: 'Arial' }}>Start</Link>
           <div className="my-2"></div>
           <Link to="/howtoplay" className="btn btn-light" style={{ padding: '1em 2em', fontSize: '1.45em', fontFamily: 'Arial' }}>How to Play</Link>
         </div>
