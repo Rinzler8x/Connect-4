5195b4b50f8330dafe11b22a370a8ef15ff83d79
Varad Kakodkar <varad25gt@gmail.com>
2024-07-13 17:17:31 +0000

    gitignore added for python

--- backend/connect_4_alpha_beta.py
+++ backend/connect_4_alpha_beta.py
@@ -8,7 +8,7 @@ import math
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["Content-Type"],
--- backend/connect_4_pvp.py
+++ backend/connect_4_pvp.py
@@ -7,7 +7,7 @@ import random
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://127.0.0.1:5173"],
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["Content-Type"],
